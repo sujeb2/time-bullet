e26b5e6784ad31abf1fab62175a3906a16a7e4e8
Sungbin Kim <sdbb2q@gmail.com>
2023-08-01 12:48:29 +0900

    똑딱똑딱

--- GameSetting.py
+++ GameSetting.py
@@ -13,7 +13,9 @@ MOTD = {
     '개발자들은 현재 자고 있을수도..',
     '이거 보고 있으신가요?',
     '여전히 시간은 가고 있어요 *똑딱똑딱*',
-    '혹시 이 메세지를 볼려고 게임을 계속 껐다가 키는건 아니죠?'
+    '혹시 이 메세지를 볼려고 게임을 계속 껐다가 키는건 아니죠?',
+    '그거 아시나요? 이 메세지는 랜덤으로 정해지는거!',
+    '누가 알겠어요, 원래 이 게임은 퍼즐게임으로 만들어졌지만 FPS로 변경된걸'
 }
 # Player Setting
 PLAYER_START_X = 150
@@ -39,7 +41,7 @@ SHOW_CURRENTFPS = True
 DEBUG_FPSWARNING_VALUE = 15
 SHOW_PLAYERMANA_CONSOLE = False
 SCREEN_FLAGS = DOUBLEBUF
-RUN_GAME_BEFORE_MENU = True
+RUN_GAME_BEFORE_MENU = False
 RUN_FULLSCREEN = False
 SHOW_TRIGGERS = False
 DRAW_GREYBACKGROUND_ASVOID = True
