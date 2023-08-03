1a88989cc69c6f0852dab1d3a842aef60efc500a
Sungbin Kim <sdbb2q@gmail.com>
2023-08-03 10:55:44 +0900

    모듈업데이트 스크립트 업데이트

--- 모듈 업데이트.bat
+++ 모듈 업데이트.bat
@@ -1,7 +1,9 @@
 @echo off
-title 모듈 업데이트
-set /p link=requirements.txt 파일 위치 입력: 
+title 모듈 업데이트 스크립트
+set /p link=requirements.txt 파일위치:  
 pip install -r %link%
+echo pip 업그레이드 중
+python.exe -m pip install --upgrade pip
 echo 성공
 pause
 exit
